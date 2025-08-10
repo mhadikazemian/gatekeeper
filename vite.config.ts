583c694f2e42b99996595b47e67c7b67fea5a7d0
hadi <hadi@peakcapitaltrading.com>
2025-08-10 22:36:39 +0330

    Make ready for GitHub deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/gatekeeper/",
   plugins: [react()],
   test: {
     globals: true, // gives you test(), describe(), expect()
